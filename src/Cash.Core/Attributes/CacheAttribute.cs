d4c03a233ffcd5bdb69e5e0bbf0009bf01264d7d
Andrew Morger <asmorger@live.com>
2016-01-22 15:33:23 -0600

    Added the cache attribute

---
+++ src/Cash.Core/Attributes/CacheAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cash.Core.Attributes
+{
+    /// <summary>
+    /// Attribute used to indicate that a method should be cached.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CacheAttribute : Attribute
+    {
+    }
+}
