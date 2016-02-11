b572b3a9d10d13ac28f8df34e3cda7711d34881d
Andrew Morger <amorger@nerdery.com>
2016-02-11 18:05:17 -0500

    Created UnitTests around the CacheAttribute class.

---
+++ src/Cash.Core.Tests/Attributes/CacheAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cash.Core.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Cash.Core.Tests.Attributes
+{
+    [TestClass]
+    public class CacheAttributeTests
+    {
+        public CacheAttribute CacheAttribute { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            CacheAttribute = new CacheAttribute();
+        }
+
+        [TestMethod]
+        public void Ctor_DefaultsThePriority_ToNormal()
+        {
+            var attribute = new CacheAttribute();
+
+            attribute.Priority.ShouldBe(CacheItemPriority.Normal);
+        }
+
+        [TestMethod]
+        public void Ctor_SetsThePriority_ToWhatIsPassedIn()
+        {
+            var attribute = new CacheAttribute(CacheItemPriority.High);
+
+            attribute.Priority.ShouldBe(CacheItemPriority.High);
+        }
+    }
+}
