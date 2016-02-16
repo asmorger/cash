9dcf27320f59760e2277ac73ad66696e42257515
Andrew Morger <asmorger@live.com>
2016-02-16 08:47:31 -0600

    Removed test exclusion attributes (didn't work) and added an exclusion filter to the OpenCover command

--- src/Cash.Core.Tests/Models/TestModelDefinition.cs
+++ src/Cash.Core.Tests/Models/TestModelDefinition.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) Andrew Morger. All rights reserved.
 // Licensed under the GNU General Public License, Version 3.0. See License.txt in the project root for license information.
 
-using System.Diagnostics.CodeAnalysis;
-
 using Cash.Core.Attributes;
 
 namespace Cash.Core.Tests.Models
 {
-    [ExcludeFromCodeCoverage]
     public class TestModelDefinition
     {
         public int Id { get; set; }
