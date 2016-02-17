0f1f0f51fb9c1626c731a8054d0c241c69a7c7af
Andrew Morger <asmorger@live.com>
2016-02-17 09:10:32 -0600

    Added missing documentation

--- src/Cash.Core.Autofac/Cash.cs
+++ src/Cash.Core.Autofac/Cash.cs
@@ -6,6 +6,11 @@ namespace Cash.Core.Autofac
 {
     public static class Cash
     {
+        /// <summary>
+        /// Adds the Cash-specific registrations to the Autofac container builder.
+        /// </summary>
+        /// <param name="builder">The Autofac <see cref="ContainerBuilder"/></param>
+        /// <param name="cacheProvider">The <see cref="ObjectCache"/> instance to use as a cache provider.</param>
         public static void RegisterCacheInfrastructure(ContainerBuilder builder, ObjectCache cacheProvider)
         {
             builder.Register(x => cacheProvider).As<ObjectCache>().SingleInstance();
