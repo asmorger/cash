daca72d8ecdf6a367cd781b33b6272b004d02d8f
Andrew Morger <asmorger@live.com>
2016-02-17 09:01:38 -0600

    Added Autofac project

---
+++ src/Cash.Core.Autofac/Cash.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Cash.Core.Services;
+using System.Runtime.Caching;
+
+namespace Cash.Core.Autofac
+{
+    public static class Cash
+    {
+        public static void RegisterCacheInfrastructure(ContainerBuilder builder, ObjectCache cacheProvider)
+        {
+            builder.Register(x => cacheProvider).As<ObjectCache>().SingleInstance();
+            builder.RegisterType<CacheKeyGenerationService>().As<ICacheKeyGenerationService>().SingleInstance();
+            builder.RegisterType<CacheKeyRegistrationService>().As<ICacheKeyRegistrationService>().SingleInstance();
+        }
+    }
+}
