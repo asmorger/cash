4278f69e0942c93d2ce3125e8447f578f4c01b13
Andrew Morger <asmorger@live.com>
2016-02-18 09:03:32 -0600

    Added Ninject test project and began adding tests

---
+++ src/Cash.Ninject.Tests/Extensions/NinjectExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Cash.Core.Services;
+using Cash.Ninject.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Ninject;
+
+namespace Cash.Ninject.Tests.Extensions
+{
+    [TestClass]
+    public class NinjectExtensionsTests
+    {
+        public StandardKernel Kernel { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Kernel = new StandardKernel();
+        }
+
+        [TestMethod]
+        public void WithDefaultCache_ExecutesSuccessfully()
+        {
+            try
+            {
+                Kernel.Bind<ICacheKeyGenerationService>().To<CacheKeyGenerationService>().InThreadScope().WithDefaultCache();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Exception occured when none was expected. Message: '{ex.Message}'");
+            }
+        }
+    }
+}
--- src/Cash.Ninject/Cash.cs
+++ src/Cash.Ninject/Cash.cs
@@ -6,13 +6,18 @@ using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 
+using Cash.Core.Services;
+
 namespace Cash.Ninject
 {
     public static class Cash
     {
         public static void RegisterCacheInfrastructure(StandardKernel kernel, ObjectCache cache)
         {
-            
+            kernel.Bind<ICacheKeyGenerationService>().To<CacheKeyGenerationService>();
+            kernel.Bind<ICacheKeyRegistrationService>().To<CacheKeyRegistrationService>();
+
+            kernel.Bind<ObjectCache>().ToConstant(cache);
         }
     }
 }
