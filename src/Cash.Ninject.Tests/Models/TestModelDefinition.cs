8c64ba1847487cc0ade86d4bbe58387c8b6ce79d
Andrew Morger <asmorger@live.com>
2016-02-18 09:13:51 -0600

    Copied the CachingInterceptor tests for the NinjectCachingInterceptor and updated the Fakes to match Ninject's structure.  Brought over needed models as well.

---
+++ src/Cash.Ninject.Tests/Models/TestModelDefinition.cs
@@ -0,0 +1,30 @@
+﻿using Cash.Core.Attributes;
+
+namespace Cash.Ninject.Tests.Models
+{
+    public class TestModelDefinition
+    {
+        public int Id { get; set; }
+
+        public void TestMethod_NoParameters()
+        {
+        }
+
+        public void TestMethod_OneSimpleParameter(int x)
+        {
+        }
+
+        public void TestMethod_TwoSimpleParameters(double height, int age)
+        {
+        }
+
+        [Cache]
+        public void TestMethod_WithCacheAttribute()
+        {
+        }
+
+        public void TestMethod_WithNoCacheAttribute()
+        {
+        }
+    }
+}
