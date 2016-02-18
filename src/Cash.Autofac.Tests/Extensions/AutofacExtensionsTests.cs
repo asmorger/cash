77dda6eec136495b63a12859ccdd6e524d15f30a
Andrew Morger <asmorger@live.com>
2016-02-18 08:33:53 -0600

    Brought the Autofac test coverage back up to 100%

---
+++ src/Cash.Autofac.Tests/Extensions/AutofacExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Autofac;
+
+using Cash.Autofac.Extensions;
+using Cash.Core.Services;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cash.Autofac.Tests.Extensions
+{
+    [TestClass]
+    public class AutofacExtensionsTests
+    {
+        public ContainerBuilder Builder { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Builder = new ContainerBuilder();
+        }
+
+        [TestMethod]
+        public void WithDefaultCache_ExecutesSuccessfully()
+        {
+            try
+            {
+                Builder.RegisterType<CacheKeyGenerationService>().As<ICacheKeyGenerationService>().WithDefaultCache();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Exception occured when none was expected. Message: '{ex.Message}'");
+            }
+        }
+    }
+}
