d64cba644c8c8c04564576a9fae717871ce7ec55
Andrew Morger <asmorger@live.com>
2016-02-23 08:10:42 -0600

    Setup sample project with Cash and integrated the Ninject MVC5 support

---
+++ samples/ninject/Cash.Ninject.Sample.Web/App_Start/CashConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cash.Core.Services;
+
+namespace Cash.Ninject.Sample.Web.App_Start
+{
+    public static class CashConfig
+    {
+        public static ICacheKeyRegistrationService RegisterCacheKeys()
+        {
+            var registrationService = new CacheKeyRegistrationService();
+
+            // registrationService.AddTypedCacheKeyProvider<UserModel>(x => $"{x.Id}");
+
+            return registrationService;
+        }
+    }
+}
