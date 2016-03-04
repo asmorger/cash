39cb36174111e88e33d443de18782d6a70835274
Andrew Morger <asmorger@live.com>
2016-03-04 09:56:11 -0600

    Fixed copy-pasta script contents from another project

--- scripts/SetPackageVersion.ps1
+++ scripts/SetPackageVersion.ps1
@@ -1,8 +1,8 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 
-@('Nap', 'Nap.Configuration', 'Nap.Html') | %{
+@('Cash.Core', 'Cash.Autofac', 'Cash.Ninject') | %{
   Write-Host "Setting .nuspec for $_ version tag to $($env:APPVEYOR_BUILD_VERSION)"
-  $content = (Get-Content $root\$_\$_.nuspec) 
+  $content = (Get-Content $root\src\$_\$_.nuspec) 
   $content = $content -replace '\$version\$',$env:APPVEYOR_BUILD_VERSION
   $content | Out-File $root\$_\$_.nuspec
 }
