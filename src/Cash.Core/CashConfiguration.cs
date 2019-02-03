da33c85e8fb8bdf1046ccdebae3b5e26a44c0c99
Anderw Morger <asmorger@live.com>
2019-02-03 11:36:51 -0600

    Added comments

--- src/Cash.Core/CashConfiguration.cs
+++ src/Cash.Core/CashConfiguration.cs
@@ -5,13 +5,28 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Cash.Core
 {
+    /// <summary>
+    /// Represents the final configuration of an item to be cached.
+    /// </summary>
     public interface ICacheConfiguration
     {
+        /// <summary>
+        /// The cache key to be used.
+        /// </summary>
         string Key { get; }
+        
+        /// <summary>
+        /// The item to be cached.
+        /// </summary>
         object Item { get; }
+        
+        /// <summary>
+        /// The options that control the caching behavior.
+        /// </summary>
         MemoryCacheEntryOptions Options { get; }
     }
     
+    /// <inheritdoc cref="ICacheConfiguration"/>
     public class CashConfiguration : ICacheConfiguration
     {
         public CashConfiguration(CacheAttribute attribute, object item, string key)
