28bf5cd69ff8832755e975eee9c472b9cc19552d
Anderw Morger <asmorger@live.com>
2019-02-03 11:58:58 -0600

    Added properties for the Sliding Expiration definition on the Cache attribute

--- src/Cash.Core/CacheAttribute.cs
+++ src/Cash.Core/CacheAttribute.cs
@@ -13,22 +13,74 @@ namespace Cash.Core
     public class CacheAttribute : Attribute
     {
         /// <summary>
-        ///     Gets the priority at which something should be cached.
+        /// The priority at which something should be cached.
         /// </summary>
-        /// <value>The priority that should be used</value>
-        public CacheItemPriority Priority { get; private set; }
+        public CacheItemPriority Priority { get; }
 
         /// <summary>
-        ///     Initializes a new instance of the <see cref="CacheAttribute" /> class.
+        /// The maximum duration that the item should be cached. 
+        /// </summary>
+        public double? Duration { get; }
+
+        /// <summary>
+        /// The time measurement by which the <see cref="Duration"/> is applied.
+        /// </summary>
+        public TimeMeasure Measure { get; }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="CacheAttribute" /> class.
         /// </summary>
         /// <param name="priority">The <see cref="CacheItemPriority" /> priority that should be applied to this method's output.</param>
         public CacheAttribute(CacheItemPriority priority = CacheItemPriority.Normal)
         {
             Priority = priority;
         }
+        
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="CacheAttribute" /> class.
+        /// </summary>
+        public CacheAttribute(double duration, TimeMeasure measure)
+        {
+            Priority = CacheItemPriority.Normal;
+            Duration = duration;
+            Measure = measure;
+        }
 
         public MemoryCacheEntryOptions GetCacheEntryOptions()
-            => new MemoryCacheEntryOptions()
-                .SetPriority(Priority);
+        {
+            TimeSpan ConvertDuration()
+            {
+                switch (Measure)
+                {
+                    case TimeMeasure.Seconds:
+                        return TimeSpan.FromSeconds(Duration.Value);
+                    case TimeMeasure.Minutes:
+                        return TimeSpan.FromMinutes(Duration.Value);
+                    case TimeMeasure.Hours:
+                        return TimeSpan.FromHours(Duration.Value);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            var options = new MemoryCacheEntryOptions
+            {
+                Priority = Priority
+            };
+
+            if (Duration.HasValue)
+            {
+                options.SlidingExpiration = ConvertDuration();
+            }
+
+            return options;
+        }
+    }
+
+    public enum TimeMeasure
+    {
+        Seconds,
+        Minutes,
+        Hours
     }
 }
